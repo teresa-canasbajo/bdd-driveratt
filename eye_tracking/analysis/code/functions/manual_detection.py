33dd7d229d86f42cc94f08a851fe310faaac27ea
Richard Liu <richard.y.liu@berkeley.edu>
2019-10-06 17:02:34 -0700

    Manual detection fixed and completed

--- eye_tracking/analysis/code/functions/manual_detection.py
+++ eye_tracking/analysis/code/functions/manual_detection.py
@@ -1,48 +1,82 @@
 from pupil_apriltags import Detector, Detection
 from collections import defaultdict
+from typing import List, Dict, Tuple, Any
 from glob import glob
+import numpy as np
 
 import cv2
 
-frames = []
-tag_ids = defaultdict(int)
-tag_count = 0
-at_detector = Detector()
+### Change paths to match yours ###
+test_folder_path = '/Users/richardliu/Documents/BDD/bdd-driveratt/eye_tracking/analysis/test' # Change filepath
+video_path = '/Users/richardliu/Downloads/world.mp4' # Change filepath
+frames_path = '/Users/richardliu/Documents/BDD/bdd-driveratt/eye_tracking/analysis/surfaceTest' # Change filepath (also make sure this directory exists)
 
-video_path = ''
+def extract_frames(video_path: str, frames_path: str) -> None:
+    """Convert a video (mp4 or similar) into a series of individual PNG frames.
+    Make sure to create a directory to store the frames before running this function.
 
-def extract_frames(video_path, frames_path):
+    Args:
+        video_path (str): filepath to the video being converted
+        frames_path (str): filepath to the target directory that will contain the extract frames
+    """
     video = cv2.VideoCapture(video_path)
     count = 0
     success = 1
 
+    # Basically just using OpenCV's tools
     while success:
         success, frame = video.read()
-        cv2.imwrite(f"{frames_path}/frame{count}.png", frame)
+        cv2.imwrite(f'{frames_path}/frame{count}.png', frame)
         count += 1
+    
+    # Optional print statement
+    print(f'Extracted {count} frames from {video_path}.')
 
-test_folder_path = '/Users/richardliu/Documents/BDD/bdd-driveratt/eye_tracking/analysis/test'
-frames_path = '/Users/richardliu/Documents/BDD/bdd-driveratt/eye_tracking/analysis/surface2frames'
+def detect_tags(frames_path: str) -> Tuple[List[Dict[str, Any]], Dict[int, int]]:
+    """Detect all tags (Apriltags3) found in a folder of PNG files and return (1) a list of tag objects
+    for preprocessing and (2) a dictionary containing the frequency that each tag ID appeared
 
-for img_path in glob(f'{test_folder_path}/*.png'):
-    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-    tags = at_detector.detect(img, estimate_tag_pose=False, camera_params=None, tag_size=None)
-    tag_count += len(tags)
-    for tag in tags:
-        tag_ids[tag.tag_id] += 1
-        frames.append({
-            'id': tag.tag_id,
-            'centroid': tag.center,
-            'verts': tag.corners
-        })
-        
+    Args:
+        frames_path (str): path to the directory containing PNG images
+    
+    Returns:
+        frames (List[Dict[str, Any]]): list of objects containing id (int), centroid (np.array[int]) and corners (np.array[int])
+        tag_ds (Dict[int, int]): dictionary mapping tag IDs to frequency of tag across all images
+    """
+    # Initialize variables
+    frames = []
+    tag_ids = defaultdict(int)
+    at_detector = Detector()
 
-# Helper function
-# def load_image_into_numpy_array(image):
-#     print(image)
-#     (im_width, im_height) = image.size
-#     return np.array(image.getdata()).reshape(
-#         (im_height, im_width, 3)).astype(np.uint8)
+    # Iterate thru all PNG images in frames_path
+    for img_path in glob(f'{frames_path}/*.png'):
+        # Createa grayscale 2D NumPy array for Detector.detect()
+        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 
+        if type(img) == np.ndarray:
+            # Optional print statement
+            print(f'Analyzing file: {img_path}')
+
+            for tag in at_detector.detect(img):
+                # Increment frequency
+                tag_ids[tag.tag_id] += 1
+
+                # Add to frames in following format - feel free to adjust
+                frames.append({
+                    'id': tag.tag_id,
+                    'centroid': tag.center,
+                    'verts': tag.corners
+                })
+
+    return frames, tag_ids
+
+# Convert video to frames
+extract_frames(video_path, frames_path)
+
+# Detect tags in frames
+frames, tag_ids = detect_tags(frames_path)
+
+# Descriptive print statements
+tag_count = sum(count for count in tag_ids.values())
 print(f'Detected {tag_count} tags in {len(frames)} frames.')
-print(f'Found IDs of f{tag_ids.keys()}')
+print(f'Found IDs of {list(tag_ids.keys())}.')
