627210718a19ddec9dd202f5301c94e3c6c4e14c
Teresa <tere93@gmail.com>
2019-11-14 15:58:32 -0800

    deleted all directly unused code, will be adding one by one as needed

--- eye_tracking/analysis/code/debug/debug_plpreprocess.py
+++ eye_tracking/analysis/code/debug/debug_plpreprocess.py
@@ -1,11 +1,6 @@
 from debug.debug_pl_surface_new import *
 from functions.et_preprocess import *
-# test path from 10/17 recording at lab with calibration
-path3 = '/home/whitney/Teresa/demos/old/surfaceANDcalibration_3screens'
-# test path from 10/25 recording
-path = '/home/whitney/Teresa/demos/3screens_1025'
-# test path from 11/4 recording
-path4 = '/home/whitney/Teresa/demos/2019_11_04/000'
+
 super_best = '/home/whitney/Teresa/demos/super_best'
 
 original = '/media/whitney/New Volume/Teresa/bdd-driveratt/VP1/raw'
