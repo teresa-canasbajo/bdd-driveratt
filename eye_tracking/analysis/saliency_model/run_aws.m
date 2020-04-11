4b2fba87f2a2d16c76656765b7e4a9239f2c29cb
klo9klo9kloi <jszou@berkeley.edu>
2020-04-11 14:40:27 -0700

    update matlab scripts

--- eye_tracking/analysis/saliency_model/run_aws.m
+++ eye_tracking/analysis/saliency_model/run_aws.m
@@ -1,10 +1,17 @@
-mkdir aws_maps
+% example of running on command line: 
+% matlab -nodisplay -nosplash -nodesktop -r "run('/home/jszou/cs/eye_tracking/bdd-driveratt/eye_tracking/analysis/saliency_model/run_aws.m'); exit;"
+%
+% mkdir aws_maps
+mkdir '/media/whitney/New Volume/Teresa/bdd-driveratt/demo/aws_maps'
 addpath('./aws/')
-to_process = dir('frames')
+% to_process = dir('frames')
+to_process = dir('/media/whitney/New Volume/Teresa/bdd-driveratt/demo/frames')
 for n=3:length(to_process)
 	filename = to_process(n).name
-	im = imread(strcat('frames/', filename))
+	% im = imread(strcat('frames/', filename))
+	im = imread(strcat('/media/whitney/New Volume/Teresa/bdd-driveratt/demo/frames/', filename))
 	smap = aws(im)
 	graymap = mat2gray(smap)
-	imwrite(graymap, strcat('aws_maps/', filename(1:length(filename)-3)), 'png')
+	% imwrite(graymap, strcat('aws_maps/', filename(1:length(filename)-3)), 'png')
+	imwrite(graymap, strcat('/media/whitney/New Volume/Teresa/bdd-driveratt/demo/aws_maps/', filename(1:length(filename)-3)), 'png')
 end
--- eye_tracking/analysis/saliency_model/run_simpsal.m
+++ eye_tracking/analysis/saliency_model/run_simpsal.m
@@ -1,11 +1,17 @@
-mkdir simpsal_maps
+% mkdir simpsal_maps
+mkdir '/media/whitney/New Volume/Teresa/bdd-driveratt/demo/simpsal_maps'
 addpath('./simpsal/')
-to_process = dir('frames')
+% to_process = dir('frames')
+to_process = dir('/media/whitney/New Volume/Teresa/bdd-driveratt/demo/frames')
 for n=3:length(to_process)
 	filename = to_process(n).name
-	im = imread(strcat('frames/', filename))
-	smap = aws(im)
+	% im = imread(strcat('frames/', filename))
+	im = imread(strcat('/media/whitney/New Volume/Teresa/bdd-driveratt/demo/frames/', filename))
+	smap = simpsal(im, default_fast_param)
 	graymap = mat2gray(smap)
 	graymap_big = imresize(graymap, [ size(im, 1), size(im, 2) ] )
-	imwrite(graymap, strcat('simpsal_maps/', filename(1:length(filename)-3)), 'png')
-	imwrite(graymap, strcat('simpsal_maps/', filename(1:length(filename)-4)), '_big.png')
+	% imwrite(graymap, strcat('simpsal_maps/', filename(1:length(filename)-3)), 'png')
+	% imwrite(graymap, strcat('simpsal_maps/', filename(1:length(filename)-4)), '_big.png')
+	imwrite(graymap, strcat('/media/whitney/New Volume/Teresa/bdd-driveratt/demo/simpsal_maps/', filename(1:length(filename)-3)), 'png')
+	imwrite(graymap, strcat('/media/whitney/New Volume/Teresa/bdd-driveratt/demo/simpsal_maps/', filename(1:length(filename)-4)), '_big.png')
+end
