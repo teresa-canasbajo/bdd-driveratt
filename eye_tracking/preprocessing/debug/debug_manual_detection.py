63a451facbacd978f2b34865738bdb18fddb13c1
dhakshi <dhakshi.balakumar@att.net>
2020-05-26 17:32:39 -0700

    manual surface detection coordinates dataframe

--- eye_tracking/preprocessing/debug/debug_manual_detection.py
+++ eye_tracking/preprocessing/debug/debug_manual_detection.py
@@ -1,9 +1,11 @@
 from eye_tracking.preprocessing.functions.manual_detection import *
 
 # demo = '/media/whitney/New Volume/Teresa/frames'
-demo = '/media/whitney/New Volume/Teresa/SD_grant_EM/003/frames'
+# demo = '/media/whitney/New Volume/Teresa/SD_grant_EM/003/frames'
+demo = '/media/whitney/New Volume/Teresa/SD_grant_EM/003_modified/frames'
 
 # frame, tag_ids = detect_tags(demo)
-frame, tag_ids = detect_tags(demo)
+frame, tag_ids, coordinates_df = detect_tags(demo)
+coordinates_df.to_csv(os.path.join(demo, 'coordinates.csv'), index=False)
 
 print('done!')
